86a208db4656fd12a6d721a8479049e2f19d9195
lifelong-AI <lifeilong2026@gmail.com>
2026-09-22 20:39:21 +0800

    Unify serif typography and expand publication author names

---
+++ _next/static/chunks/app/layout-a61aa07d0ae9415d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{1076:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,4781,23))},4781:()=>{}},e=>{e.O(0,[818,951,79,358],()=>e(e.s=1076)),_N_E=e.O()}]);
